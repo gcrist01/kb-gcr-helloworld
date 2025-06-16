46f4c5caf6be393adcd3dfccccd60197acef8592
gcrist01 <gerard.cristofolroig@kiwibank.co.nz>
2025-06-17 11:18:54 +1200

    remove pipeline from templated pipeline

--- MinimalWebApp3/.pipelineconfig/pipeline_config.groovy
+++ MinimalWebApp3/.pipelineconfig/pipeline_config.groovy
@@ -1,11 +1,3 @@
 libraries {
     dotnet
 }
-
-steps {
-    build {
-        cakeScript = "/home/jenkins/template-run/cake/build.min.cake"
-        image = 'fooapp:latest'
-        bar = 'baz'
-    }
-}
