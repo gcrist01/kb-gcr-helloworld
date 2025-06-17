e49bb403c7f6d89ec704757acde450457b312d89
gcrist01 <gerard.cristofolroig@kiwibank.co.nz>
2025-06-17 13:26:30 +1200

    pipeline implementation

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -1,3 +1,10 @@
 libraries {
-    dotnet
+    dotnet{
+        //Hack the paths a little bit
+        KB_SCRIPT_PATH = "MinimalWebApp3/.pipelineconfig"
+        KB_PROJECT_PATH = "MinimalWebApp3"
+        //PROJECT_NAME = 'MinimalWebApp'
+        //DOCKER_IMAGE = 'minimal-webapp'
+    }
 }
+
